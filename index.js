62fbd13280c825e8b66a6c9dfd91908b80c6c9c1
Ryan Hanzel <100760416+H3Xivall@users.noreply.github.com>
2024-01-22 16:43:56 -0600

    added the prompt function and then(answers)

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 import inquirer from "inquirer";
-import jest from "jest";
 import shapes from "./shapes";
 
 const prompt = inquirer.createPromptModule();
+const Triangle = new shapes.Triangle();
+const Cirle = new shapes.Circle();
+const Square = new shapes.Square();
 const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three letters',
+        validate: function(input) {
+            if(input.length > 3) {
+                return 'Please enter up to three letters';
+            }
+            return true;
+        }
+    }, {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape',
+        choices: ['Circle', 'Square', 'Triangle']
+    }, {
+        type: 'input',
+        name: 'color',
+        message: 'Enter a color keyword or hexadecimal number'
+    }
+];
 
-]
