910f0b05617bd7a324544eca1eaec9086cb59ac2
Ryan Hanzel <100760416+H3Xivall@users.noreply.github.com>
2024-02-25 22:37:47 -0600

    Made some changes to the entirety of the code

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-import inquirer from "inquirer";
-import shapes from "./shapes";
+// index
+import shapes from "./lib/shapes.js";
+const inquirer = require("inquirer");
 
 const prompt = inquirer.createPromptModule();
 const Triangle = new shapes.Triangle();
 const Circle = new shapes.Circle();
 const Square = new shapes.Square();
 const questions = [
-    {
-        type: 'input',
-        name: 'text',
-        message: 'Enter up to three letters',
-        validate: function(input) {
-            if(input.length > 3) {
-                return 'Please enter up to three letters';
-            }
-            return true;
-        }
-    }, {
-        type: 'list',
-        name: 'shape',
-        message: 'Choose a shape',
-        choices: ['Circle', 'Square', 'Triangle']
-    }, {
-        type: 'input',
-        name: 'color',
-        message: 'Enter a color keyword or hexadecimal number'
-    }
+  {
+    type: "input",
+    name: "text",
+    message: "Enter up to three letters",
+    validate: function (input) {
+      if (input.length > 3) {
+        return "Please enter up to three letters";
+      }
+      return true;
+    },
+  },
+  {
+    type: "list",
+    name: "shape",
+    message: "Choose a shape",
+    choices: ["Circle", "Square", "Triangle"],
+  },
+  {
+    type: "input",
+    name: "color",
+    message: "Enter a color keyword or hexadecimal number",
+  },
 ];
 
-prompt(questions).then(answers => {
-    if(answers.shape === 'Circle') {
-        Circle(answers.text, answers.color);
-    } else if(answers.shape === 'Square') {
-        Square(answers.text, answers.color);
-    } else if(answers.shape === 'Triangle') {
-        Triangle(answers.text, answers.color);
-    } else {
-        console.log('Invalid shape');
-    };
-    
-})
+prompt(questions).then((answers) => {
+  if (answers.shape === "Circle") {
+    Circle(answers.text, answers.color);
+  } else if (answers.shape === "Square") {
+    Square(answers.text, answers.color);
+  } else if (answers.shape === "Triangle") {
+    Triangle(answers.text, answers.color);
+  } else {
+    console.log("Invalid shape");
+  }
+});
+
